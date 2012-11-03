7b75dbac9a00119b7ade036d040c03fd241b122b
Jerome Morin-Drouin <jmdrouin@gmail.com>
2012-11-04 00:05:35 +0100

    Test laziness of Enumerator#where

--- incredible_enumerators_spec.rb
+++ incredible_enumerators_spec.rb
@@ -3,8 +3,20 @@
 require "./incredible_enumerators.rb"
 
 describe IncredibleEnumerator do
-  it "can avoid some values using #where" do
-    enum = (0...10).each.where(&:even?)
-    enum.to_a.should == [0,2,4,6,8]
+  describe "#where" do
+    it "filters the enumeration following the predicate" do
+      enum = (0...10).each.where(&:even?)
+      enum.to_a.should == [0,2,4,6,8]
+    end
+
+    it "runs the filter only when needed" do
+      has_been_called = false
+
+      enum = (0...10).each.where {has_been_called = true}
+      has_been_called.should == false
+
+      enum.to_a
+      has_been_called.should == true
+    end
   end
 end
